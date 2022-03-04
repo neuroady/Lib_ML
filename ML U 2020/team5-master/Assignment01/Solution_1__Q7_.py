7056e99a928aa3cdcb02d8bbd4c39a95371a2156
Ady Sharma <65286024+neuroady@users.noreply.github.com>
2022-03-04 14:17:48 +0100

    Add all of GitLab repository contents

---
+++ ML U 2020/team5-master/Assignment01/Solution_1__Q7_.py
@@ -0,0 +1,28 @@
+""" This file is referenced in the Jpyter Notebook: question_04.ipynb """
+
+
+import numpy as np
+
+# [Q7.1] Compute the matriX product of two matrices
+X = np.array([ [5, 8], [2, 1], [6, 7] ])
+y = np.array([ [5, 8, 3], [2, 1, 9] ])
+
+
+print("Q7.1\nX:\n", X, "\n")
+print("\nY:\n", y , "\n\n")
+
+print("Shapes:\n")
+print(" X : ", X.shape )
+print("\n Y : ", y.shape)
+Z = np.dot(X, y)
+print("\n\n X.Y = \n", Z)
+
+
+# [Q7.2] Find the Eigen values of a matriX
+del X
+X = np.array( [ [1, 2], [2, 4] ])
+eigVal_X, eigVec_X = np.linalg.eig(X)
+
+print("\nQ7.2\nThe matrix is :\n", X)
+print("\nEigen values of the matrix are:\n", eigVal_X)
+print("\nEigen vector of the matrix is :\n", eigVec_X)
